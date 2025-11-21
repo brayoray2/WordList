c70c3fcf0bf74c7bfeb4e094cf07e3447d22d541
Brady Ray <Brady Ray>
2025-11-21 14:51:59 -0500

    Final Update

--- words.py
+++ words.py
@@ -2,24 +2,23 @@ import numpy as np
 data = np.loadtxt('loop4.csv', delimiter=',', dtype=str, encoding = 'utf8')
 def num_letters(num_rows,num_col):
     return(num_rows+num_col)*2 - 4
-assert num_letters(3,4) == 10
+assert num_letters(3,4) == 10 
 words = []
 rows,columns = data.shape
 c = 0
 while c < columns:
-    words.append(data[0,c])
+    words.append(str(data[0,c]))
     c += 1
 r = 1
 while r < rows:
-    words.append(data[r,c-1])
+    words.append(str(data[r,c-1]))
     r += 1
 c = columns - 2
 while c >= 0:
-    words.append(data[rows - 1,c])
+    words.append(str(data[r-1,c]))
     c -= 1
 r = rows - 2
 while r > 0:
-    words.append(data[r,0])
+    words.append(str(data[r,0]))
     r -= 1
 print(words)
-    
